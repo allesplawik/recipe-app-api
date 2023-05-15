021fb2f581693bee435319dd51e623d4b6ec81cb
Sławek <slawek@192.168.1.13>
2023-05-15 19:51:45 +0200

    Docker ignore fixing

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from . import calc
+
+class CalcTestCase(SimpleTestCase):
+    """Test calc module."""
+    def test_add_number(self):
+        """Test adding nu,bers together."""
+        x = 1
+        y = 2
+
+        res = calc.add(x, y)
+
+        self.assertEqual(res, 3)
