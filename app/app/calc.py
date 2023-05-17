06fa1fdea666ac667186253587e525d45f6d42c9
Sławek <slawek@192.168.1.13>
2023-05-17 13:56:31 +0200

    Add postgresql to project

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,2 +1,7 @@
 def add(x: int, y: int) -> int:
     return x + y
+
+
+def even_numbers(numbers: list[int]) -> list[int]:
+    # return [number for number in numbers if number % 2 == 0]
+    return []
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 
 from . import calc
+from .calc import even_numbers
 
 
 class CalcTestCase(SimpleTestCase):
@@ -14,3 +15,17 @@ class CalcTestCase(SimpleTestCase):
         res = calc.add(x, y)
 
         self.assertEqual(res, 3)
+
+    def test_list_retun_even_numbers(self):
+
+        # given
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        expected_numbers = [2, 4, 6, 8, 10]
+
+        # when
+        res = even_numbers(numbers)
+
+        # then
+        self.assertEqual(res, expected_numbers)
+
+
